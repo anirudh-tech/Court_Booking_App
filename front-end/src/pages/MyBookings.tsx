2f67bd968bb65de81e91f9b79edc16d2c87fd7b3
Aflu <mohamedaflah186@gmail.com>
2024-06-20 09:44:04 +0530

    feat: payment status completed

--- front-end/src/pages/MyBookings.tsx
+++ front-end/src/pages/MyBookings.tsx
@@ -1,3 +1,4 @@
+import { PaymentProcessing } from "@/components/custom/PaymentProcess";
 import { PaymentCompleted } from "@/components/custom/Paymentcomplete";
 import { axiosInstance } from "@/constants/axiosInstance";
 import { RootState } from "@/redux/store";
@@ -9,26 +10,33 @@ import { useSelector } from "react-redux";
 
 export function MyBooking() {
   const { user } = useSelector((state: RootState) => state.user);
-  const [bookings, setBookings] = useState<Booking[]>([])
+  const [bookings, setBookings] = useState<Booking[]>([]);
   useEffect(() => {
     if (user) {
-      axiosInstance
-        .get(`/user-bookings-list/${user?._id}`)
-        .then((res) => {
-          console.log(res)
-          setBookings(res.data.data)
-        })
+      axiosInstance.get(`/user-bookings-list/${user?._id}`).then((res) => {
+        console.log(res);
+        setBookings(res.data.data);
+      });
     }
-  },[user])
+  }, [user]);
   return (
     <motion.main className="w-full min-h-screen">
-      <section className="w-[90%] mx-auto grid gap-4 xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 ">
-      {bookings?.map((booking, index) => (
+      <section className="w-[90%] mx-auto grid gap-4 xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 ">
+        {bookings?.map((booking, index) => (
           <div key={index} className="w-full flex justify-center">
             <div className="min-h-96 space-y-4 rounded-xl py-3 shadow-md sm:w-full border px-2 w-[80%] ">
               <div className="w-full px-3 py-5 border-b">
                 <div className="w-full flex justify-center flex-col gap-2 ">
-                  <PaymentCompleted />
+                  {booking?.paymentStatus == "Success" ? (
+                    <PaymentCompleted />
+                  ) : booking?.paymentStatus == "Pending" ? (
+                    <PaymentProcessing />
+                  ) : booking.paymentStatus == "Failed" ? (
+                    <PaymentProcessing />
+                  ) : (
+                    ""
+                  )}
+
                   <div className="w-full flex justify-center text-[14px] font-semibold">
                     ₹ {booking.amount}
                   </div>
@@ -36,12 +44,16 @@ export function MyBooking() {
               </div>
               <div className="w-full flex flex-col px-2 gap-2">
                 <div className="w-full flex justify-between text-[14px]">
-                  <span className="text-[#474747]">Court name</span>
-                  <span className="">{booking.courtId.courtName}</span>
+                  <span className="text-[#474747]">Court -</span>
+                  <div>
+                    <span className="">{booking.courtId.courtName}</span>
+                  </div>
                 </div>
                 <div className="w-full flex justify-between text-[14px]">
                   <span className="text-[#474747]">Date</span>
-                  <span className="">{booking.date && format(String(booking?.date), "PPP")}</span>
+                  <span className="">
+                    {booking.date && format(String(booking?.date), "PPP")}
+                  </span>
                 </div>
                 <div className="w-full flex justify-between text-[14px]">
                   <span className="text-[#474747]">Payment method</span>
